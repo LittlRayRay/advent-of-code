bb00601b762c0fb84f96c7d47e096f21c62d887a
The Average Avocado <rayan.nadeem@outlook.com>
2021-12-08 21:04:40 +0000

    gh copilot assisstance

--- day5/test.py
+++ day5/test.py
@@ -1,1 +1,7 @@
-# this is showing up
+# ask user for a negative number until they enter a negative number
+
+while(x < 0):
+    x = int(input("Enter a negative number: "))
+    print(x)
+
+# Written by: Rayan Nadeem (08/12/2021) assisted by GitHub Copilot
